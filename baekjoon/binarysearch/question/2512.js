4bc9b49ec535710ad284cc54d6e8c720ddf51c3f
devkoow <qyaka178@naver.com>
2024-05-25 19:12:35 +0900

    2024-05-25 이진탐색 1문제 업데이트

--- baekjoon/binarysearch/question/2512.js
+++ baekjoon/binarysearch/question/2512.js
@@ -1,30 +1,35 @@
 // 예산
 // 난이도 ⭐️⭐️
+// 배정된 총 예산이 조건을 만족한다면, 상한 금액을 증가(최대화가 목표)시킨다.
+// 배정된 총 예산이 조건을 만족하지 못한다면, 상한 금액을 감소시킨다.
 
-const fs = `4
-120 110 140 150
-485`;
+const fs = `5
+70 80 30 40 100
+450`;
 const input = fs.split('\n');
+const n = Number(input[0]); // N 지방 수
+const arr = input[1].split(' ').map(Number); // 지방의 요청
+const m = Number(input[2]); // M 국가예산
 
-const n = Number(input[0]);
-const arr = input[1].split(' ').map(Number);
-const m = Number(input[2]);
-
+// 상한액의 최솟값과 최댓값 초기화
+// 반복문 안에서 중앙값과 조건문 설정하기
 let start = 1;
 let end = arr.reduce((a, b) => Math.max(a, b));
-let result = 0;
+let result = 0; // 구할 상한액
+
 while (start <= end) {
-  let mid = parseInt((start + end) / 2);
-  let total = 0;
+  let mid = parseInt((start + end) / 2); // 현재 중간점(상한액)
+  let total = 0; // 예산 배정
   for (x of arr) {
-    total += Math.min(mid, x);
+    total += Math.min(x, mid);
   }
 
+  // 국가예산과 비교
   if (total <= m) {
     result = mid;
     start = mid + 1;
   } else {
-    end = mid - 1;
+    end = mid - 1; // 상한액 감소
   }
 }
 console.log(result);
---
+++ baekjoon/binarysearch/question/2802.js
@@ -0,0 +1,29 @@
+// 나무 자르기
+// 난이도 ⭐️⭐️
+// 적어도 M미터의 나무를 집에 가져가기 위해서 절단기에 설정할 수 있는 높이의 최댓값
+// 낮게 설정하면 안 되고, 딱 맞게 설정
+// 예제 정답: 36 (잘린 길이 : 0, 6, 4, 0, 10)
+
+const fs = `5 20
+4 42 40 26 46`;
+const input = fs.split('\n');
+const n = input[0].split(' ').map(Number)[0]; // 나무의 수
+const m = input[0].split(' ').map(Number)[1]; // 집으로 가지고 갈 나무 길이
+const arr = input[1].split(' ').map(Number); // 나무들 길이
+
+// 높이의 최솟값과 최댓값 초기화
+let start = 0;
+let end = arr.reduce((a, b) => Math.max(a, b));
+let result = 0; // 구해야 할 높이
+
+while (start <= end) {
+  let mid = parseInt((start + end) / 2); // 절단기 높이
+  let total = 0; // 주운 나무
+  for (x of arr) if (x > mid) total += x - mid; // 더 크면 자르고 주움
+  if (total < m) end = mid - 1; // 주운 나무가 더 적으면 높이 내림
+  else {
+    result = mid;
+    start = mid + 1;
+  }
+}
+console.log(result);
