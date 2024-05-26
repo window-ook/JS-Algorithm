b4d12f8e41b7d9fd81f895380a26aa1acbf06320
devkoow <qyaka178@naver.com>
2024-05-26 21:26:59 +0900

    2024-05-26 이진 탐색 1문제 업데이트

---
+++ baekjoon/binarysearch/question/1654.js
@@ -0,0 +1,35 @@
+// 랜선 자르기
+// 난이도 ⭐️⭐️
+// n개보다 많이 만들어도 된다
+// n개를 만들 수 있는 길이 중 최대의 길이를 구해야 한다
+
+const fs = `4 11
+802
+743
+457
+539`;
+const input = fs.split('\n');
+const k = input[0].split(' ')[0]; // 이미 가지고 있는 랜선 갯수
+const n = input[0].split(' ')[1]; // 필요한 랜선 갯수
+const arr = []; // 이미 가지고 있는 랜선들의 길이
+for (let i = 1; i <= k; i++) {
+  arr.push(Number(input[i]));
+}
+
+let start = 1; // 안 자르고는 못 만듦
+let end = arr.reduce((a, b) => Math.max(a, b));
+let result = 0;
+
+while (start <= end) {
+  let mid = parseInt((start + end) / 2); // 자를 단위
+  let total = 0; // 만들어지는 랜선 갯수
+  for (x of arr) total += parseInt(x / mid);
+
+  // total이 n보다 작으면 mid를 내려야 갯수를 늘릴 수 있음
+  if (total < n) end = mid - 1;
+  else {
+    result = mid;
+    start = mid + 1;
+  }
+}
+console.log(result);
--- baekjoon/binarysearch/question/re.js
+++ baekjoon/binarysearch/question/re.js
@@ -1,20 +1,25 @@
-const fs = `5 20
-4 42 40 26 46`;
+const fs = `4 11
+802
+743
+457
+539`;
 const input = fs.split('\n');
-const n = input[0].split(' ').map(Number)[0]; // 나무의 수
-const m = input[0].split(' ').map(Number)[1]; // 집으로 가지고 갈 나무 길이
-const arr = input[1].split(' ').map(Number); // 나무들 길이
+const k = Number(input[0].split(' ')[0]);
+const n = Number(input[0].split(' ')[1]);
+const arr = [];
+for (let i = 1; i <= k; i++) {
+  arr.push(Number(input[i]));
+}
 
-let start = 0;
+let start = 1;
 let end = arr.reduce((a, b) => Math.max(a, b));
 let result = 0;
 
 while (start <= end) {
-  let mid = parseInt((start + end) / 2);
   let total = 0;
-  for (x of arr) if (x > mid) total += x - mid;
-  // 높이를 올리면 total이 줄어들고, 반대는 total이 늘어남
-  if (total < m) end = mid - 1;
+  let mid = parseInt((start + end) / 2);
+  for (x of arr) total += parseInt(x / mid);
+  if (total < n) end = mid - 1;
   else {
     result = mid;
     start = mid + 1;
