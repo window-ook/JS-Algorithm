d27e5233b845862948cdc0841d748905fd6f7c53
devkoow <qyaka178@naver.com>
2024-08-08 14:09:57 +0900

    15649번 풀이 수정

--- baekjoon/backtracking/exam/15649.js
+++ baekjoon/backtracking/exam/15649.js
@@ -8,7 +8,7 @@ N! (중복 불가능)
 3. 자료구조
 - arr[] 순열을 계산할 원소를 담은 배열 : int
 - visited[] 방문 여부 : boolean
-- selected[] 현재 원소의 인덱스 : int*/
+- selected[] 현재 원소의 인덱스 : int */
 
 const input = `4 2`;
 const [n, m] = input.split(' ').map(Number);
@@ -19,21 +19,21 @@ let selected = []; // 현재 인덱스 추가
 let answer = '';
 
 function dfs(arr, depth) {
-  // depth 2
+  // depth가 m이랑 같으면 이전 depth에 선택된 인덱스를 arr에서 찾아 순열로 저장해서 정답에 포함시켜야 함
   if (depth == m) {
     let result = []; // 순열 결과 저장 테이블
-    for (i of selected) result.push(arr[i]); // result에 순열 저장
-    for (x of result) answer += x + ' '; // 정답에 순열을 저장
-    answer += '\n'; // 1 2 (줄바꿈)
-    return; // 이전 호출 단계로 돌아가기 = 이전 depth로 돌아감 = 백트래킹
+    for (i of selected) result.push(arr[i]); // selected에 넣어둔 arr의 인덱스에 해당하는 숫자들을 result에 넣기
+    for (x of result) answer += x + ' '; // 정답에 result에 있는 하나의 순열을 저장
+    answer += '\n'; // 줄바꿈
+    return; // 이전 depth로 돌아감
   }
 
   for (let i = 0; i < arr.length; i++) {
-    if (visited[i]) continue;
-    selected.push(i); // depth 0 : 1, depth 1 : 2 -> 3 -> 4
-    visited[i] = true; // 방문 여부 표시
-    dfs(arr, depth + 1); // 다음 depth로
-    selected.pop(); // return으로 돌아오면 실행됨. [1, 2]였다면 다시 [1]이 되는 것
+    if (visited[i]) continue; // (1,1) / (2,2) 방지용으로 넘어가게 함
+    selected.push(i); // 순열에 넣을 arr의 인덱스
+    visited[i] = true; // 방문 처리 : 같은 숫자 반복 방지
+    dfs(arr, depth + 1); // 다음 depth를 재귀 호출
+    selected.pop(); // 순열 인덱스에서 가장 마지막 인덱스 제거
     visited[i] = false; // 해당 인덱스를 방문한 곳에서 제거
   }
 }
