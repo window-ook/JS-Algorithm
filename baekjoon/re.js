8c7e5e4fbfcc6d44151a6efe65b08b848d67a7c5
devkoow <qyaka178@naver.com>
2024-08-11 22:50:53 +0900

    2024-08-11 도영이가 만든 음식 복습

--- baekjoon/re.js
+++ baekjoon/re.js
@@ -1,57 +1,40 @@
-// 외판원 순회
-// 1 -> 2 -> 3 -> 4
-// 1 -> 3 -> 2 -> 4
-// 1 -> 2 -> 4 -> 3
-// 1 -> 3 -> 4 -> 2
-const fs = `4
-0 10 15 20
-5 0 9 10
-6 13 0 12
-8 8 9 0`;
-// 0
-// 0 10 15 20
-// 0 5 0 9 10
-// 0 6 13 0 12
-// 0 8 8 9 0
+// 신맛과 쓴맛의 차이가 가장 적은 음식
+// 신맛 x, 쓴맛 y
+const fs = `2
+3 8
+5 8`;
 const input = fs.split('\n');
 const n = Number(input[0]);
-let graph = []; // 비용 그래프
-for (let i = 0; i <= n; i++) graph.push([0]);
+let arr = [];
 for (let i = 1; i <= n; i++) {
-  line = input[i].split(' ').map(Number);
-  for (let j = 0; j < n; j++) graph[i].push(line[j]);
+  let [x, y] = input[i].split(' ').map(Number);
+  arr.push([x, y]);
 }
-let visited = new Array(11).fill(false); // 이미 방문한 도시 표시
-let result = []; // 지나온 도시의 배열
-let minValue = 1e9; // 최소 비용을 비교하며 갱신해나가기
+let result = [];
+let visited = new Array(n).fill(false);
+let answer = 1e9;
 
-function dfs(depth) {
-  // depth = 0 : 1번 도시
-  if (depth == n - 1) {
-    let totalCost = 0;
-    let currNode = 1;
-    // 1번 -> 2번째 도시 -> 3번째 도시 -> 마지막 도시
-    for (let i = 0; i < n - 1; i++) {
-      let nextNode = result[i];
-      let cost = graph[currNode][nextNode];
-      if (cost == 0) return;
-      totalCost += cost;
-      currNode = nextNode;
+function dfs(depth, start) {
+  if (depth >= 1) {
+    let totalX = 1;
+    let totalY = 0;
+    for (let i of result) {
+      let [x, y] = arr[i];
+      totalX *= x;
+      totalY += y;
     }
-    // 마지막 도시 -> 1번으로 복귀
-    let cost = graph[currNode][1];
-    totalCost += cost;
-    minValue = Math.min(minValue, totalCost);
+    answer = Math.min(answer, Math.abs(totalX - totalY));
   }
 
-  for (i = 2; i <= n; i++) {
+  for (let i = start; i < n; i++) {
     if (visited[i]) continue;
     result.push(i);
     visited[i] = true;
-    dfs(depth + 1);
+    dfs(depth + 1, i + 1);
     result.pop();
     visited[i] = false;
   }
 }
-dfs(0);
-console.log(minValue);
+
+dfs(0, 0);
+console.log(answer);
