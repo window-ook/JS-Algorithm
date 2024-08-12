d5fa9b74ba78d33f8c41c40eed7e1d6968dedefe
devkoow <qyaka178@naver.com>
2024-08-12 15:46:10 +0900

    2024-08-12 6603 로또 업데이트

---
+++ baekjoon/backtracking/exam/6603.js
@@ -0,0 +1,53 @@
+/** 로또 - 난이도 ⭐️⭐️⭐️ 
+ 1. 아이디어 : 49까지의 수 중에서 k개의 수를 골라서 집합을 만든다
+ - 집합에 주어진 숫자로 6개를 선택하여 만들 수 있는 모든 집합을 구한다
+ - 순열 (숫자 중복 사용 X, 순서가 달라도 사용한 숫자의 종류가 같으면 동일한 조합)
+
+ 2. 시간복잡도 : O(C(n,k)) - n개 중에서 k개를 선택해서 조합
+
+ 3. 자료구조 : 
+ arr [] - 집합 S를 담을 배열
+ selected [] - arr의 인덱스로 사용할 숫자 구하기
+ visited [] - 방문 처리
+ result [] - 문자열로 나타내기 전의 조합
+ answer '' - 하나의 조합
+ */
+
+const fs = `7 1 2 3 4 5 6 7
+ 8 1 2 3 5 8 13 21 34
+ 0`;
+const input = fs.split('\n');
+for (let i = 0; i < input.length; i++) {
+  let data = input[i].split(' ').map(Number);
+  if (data[0] == 0)
+    break; // 입력값 마지막 줄 0 -> 테스트 케이스 종료 조건
+  else {
+    n = data[0]; // 집합 S의 숫자 개수인 K
+    arr = data.slice(1); // arr = 현재 K에 대한 집합 S
+    visited = new Array(n).fill(false); // 방문 처리
+    selected = []; // 집합 S의 모든 조합을 완전 탐색
+    answer = ``; // 모든 조합
+    dfs(arr, 0, 0);
+    console.log(answer); // 집합 하나의 모든 조합을 구한 후에는 한 줄 띄우기
+  }
+}
+
+function dfs(arr, depth, start) {
+  // 6개가 모이면 조합 출력
+  if (depth == 6) {
+    let result = []; // 집합 S로 만든 조합 1줄
+    for (let i of selected) result.push(arr[i]); // 조합에 인덱스에 해당하는 집합의 수를 집어넣기
+    for (let x of result) answer += x + ' '; // 조합을 문자열로 바꾸기
+    answer += '\n'; // 조합하나가 출력되면 줄바꿈
+    return;
+  }
+
+  for (let i = start; i < arr.length; i++) {
+    if (visited[i]) continue;
+    selected.push(i);
+    visited[i] = true;
+    dfs(arr, depth + 1, i + 1);
+    selected.pop();
+    visited[i] = false;
+  }
+}
--- baekjoon/re.js
+++ baekjoon/re.js
@@ -1,41 +1,37 @@
-// 0 만들기
-//   1부터 n까지의 숫자들의 합이 0이 되도록 만들어야 함
-//   - dfs 함수 만들기
-//   - 반복문으로 각 testCase에 해당하는 n을 dfs 실행한 값을 출력하기
-//   - 연산자를 중복해서 사용가능하기 때문에 중복순열 : visited 사용 X
-
-// 테스트 케이스 개수
-// 1 ~ 3 수열
-// 1 ~ 7 수열
-const fs = `2
-3 
-7`;
+const fs = `7 1 2 3 4 5 6 7
+ 8 1 2 3 5 8 13 21 34
+ 0`;
 const input = fs.split('\n');
-const testCase = Number(input[0]);
-let arr = [];
-for (let tc = 1; tc <= testCase; tc++) {
-  n = Number(input[tc]);
-  arr = [];
-  for (let i = 1; i <= n; i++) arr.push(i); // 현재 테스트 케이스에 필요한 수열 만들기
-  dfs([], 0);
-  console.log(); // 1개 테스트케이스 완료 후 줄바꿈
+for (let i = 0; i < input.length; i++) {
+  let data = input[i].split(' ').map(Number);
+  if (data[0] == 0) break;
+  else {
+    n = data[0];
+    arr = data.slice(1);
+    selected = [];
+    visited = new Array(n).fill(false);
+    answer = '';
+    dfs(arr, 0, 0);
+    console.log(answer);
+  }
 }
 
-function dfs(result, depth) {
-  //  n-1이 인덱스와 맞음
-  if (depth == n - 1) {
-    let str = '';
-    for (let i = 0; i < n - 1; i++) str += arr[i] + result[i]; // 숫자 + 연산자를 문자열에 붙임
-    str += arr[n - 1] + ''; // 수열의 마지막 숫자도 더해줘야함
-    current = eval(str.split(' ').join('')); // 문자열로 표현된 수식을 계산해서 결과 구하기
-    if (current == 0) console.log(str); // 수식의 결과가 0이면 출력;
-    return; // 루프 종료
+function dfs(arr, depth, start) {
+  // 숫자 6개를 사용하는것과 같으므로 조합 출력
+  if (depth == 6) {
+    let result = [];
+    for (let i of selected) result.push(arr[i]);
+    for (let x of result) answer += x + ' ';
+    answer += '\n';
+    return;
   }
 
-  // 연산자는 이어붙이기, +, - 3가지가 존재
-  for (let x of [' ', '+', '-']) {
-    result.push(x);
-    dfs(result, depth + 1);
-    result.pop();
+  for (let i = start; i < arr.length; i++) {
+    if (visited[i]) continue; // 중복 사용 방지
+    selected.push(i); // 현재 인덱스 기록
+    visited[i] = true;
+    dfs(arr, depth + 1, i + 1);
+    selected.pop();
+    visited[i] = false;
   }
 }
