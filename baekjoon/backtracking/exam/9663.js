f89667d3cefeaba8fd3b3a80825ddbe648310d1f
devkoow <qyaka178@naver.com>
2024-08-13 13:42:58 +0900

    2024-08-13 N-queen 복습

---
+++ baekjoon/backtracking/exam/9663.js
@@ -0,0 +1,38 @@
+/** N-Queen - 난이도 ⭐️⭐️⭐️
+  1. 아이디어 :
+  - 퀸을 서로 공격할 수 없게 놓는 문제
+  - 행, 열, 대각선에 위치하는지 판단하는 함수 possible(x, y)
+  - 행(기피)을 하나씩 내려가면서 확인하는 재귀함수 dfs(row)
+
+  2. 시간복잡도 : N! (중복 불가능)
+
+  3. 자료구조:
+  queens [] - 퀸 좌표
+  count - 배치 경우의 수 
+*/
+
+const fs = `8`;
+let n = Number(fs);
+let queens = [];
+let count = 0;
+
+function possible(x, y) {
+  for (let [a, b] of queens) {
+    if (x == a || y == b) return false; // 행, 열 비교
+    if (Math.abs(x - a) == Math.abs(y - b)) return false; // 대각선 비교
+  }
+  return true;
+}
+
+function dfs(row) {
+  if (row == n) count += 1; // 모든 행을 확인하고 난 뒤 경우의 수 1 추가
+  // row : 행, i : 열 인덱스
+  for (let i = 0; i < n; i++) {
+    if (!possible(row, i)) continue; // 놓을 수 없으면 현 좌표 통과
+    queens.push([row, i]); // 가능하면 현 좌표 queens에 추가
+    dfs(row + 1);
+    queens.pop();
+  }
+}
+dfs(0);
+console.log(count);
--- baekjoon/re.js
+++ baekjoon/re.js
@@ -1,34 +1,34 @@
-const fs = `7 1 2 3 4 5 6 7
- 8 1 2 3 5 8 13 21 34
- 0`;
-const input = fs.split('\n');
-for (let i = 0; i < input.length; i++) {
-  let data = input[i].split(' ').map(Number);
-  if (data[0] == 0) break;
-  k = data[0];
-  arr = data.slice(1);
-  selected = [];
-  visited = new Array(k).fill(false);
-  answer = '';
-  dfs(arr, 0, 0);
-  console.log(answer);
+/**
+ 1. 아이디어 : 퀸을 서로 공격할 수 없는 위치에 놓기
+ - N이 주어지면 N x N의 체스판이 생성된다
+ - 대각선이나 같은 행 또는 열에 있는지 판단하는 함수 possible(x, y)
+ - 행을 내려가면서 possible로 확인하는 재귀 dfs(row)
+
+ 3. 자료구조:
+ queens [] - 퀸 좌표
+ count - 좌표에 나타낼 퀸
+ */
+const fs = `8`;
+const n = Number(fs);
+let queens = [];
+let count = 0;
+
+function possible(x, y) {
+  for (let [a, b] of queens) {
+    if (a == x || b == y) return false;
+    if (Math.abs(x - a) == Math.abs(y - b)) return false;
+  }
+  return true;
 }
 
-function dfs(arr, depth, start) {
-  if (depth == 6) {
-    let result = [];
-    for (let i of selected) result.push(arr[i]);
-    for (let i of result) answer += i + ' ';
-    answer += '\n';
-    return;
-  }
-
-  for (let i = start; i < arr.length; i++) {
-    if (visited[i]) continue;
-    selected.push(i);
-    visited[i] = true;
-    dfs(arr, depth + 1, i + 1);
-    selected.pop();
-    visited[i] = false;
+function dfs(row) {
+  if (row == n) count += 1;
+  for (let i = 0; i < n; i++) {
+    if (!possible(row, i)) continue;
+    queens.push([row, i]);
+    dfs(row + 1);
+    queens.pop();
   }
 }
+dfs(0);
+console.log(count);
