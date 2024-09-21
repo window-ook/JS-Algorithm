3c038fec656cd4afa8d5e8764eca1298727a1493
devkoow <qyaka178@naver.com>
2024-09-21 13:08:47 +0900

    2024-09-21 14888 연산자 끼워넣기 업데이트

--- baekjoon/dfs/exam/10819.js
+++ baekjoon/dfs/exam/10819.js
@@ -9,17 +9,16 @@ const fs = `6
 const input = fs.split('\n');
 const n = Number(input[0]);
 let arr = input[1].split(' ').map(Number);
-let visited = new Array(10).fill(false);
+let visited = new Array(n).fill(false);
 let result = [];
 let maxValue = -1e9;
 
 function dfs(depth) {
-  // 순열
-  if (depth == n) {
-    let current = 0; // 공식 계산
+  if (depth === n) {
+    let current = 0;
     for (let i = 0; i < n - 1; i++)
-      current += Math.abs(result[i] - result[i + 1]); // 공식 계산
-    maxValue = Math.max(maxValue, current); // 최대값
+      current += Math.abs(result[i] - result[i + 1]);
+    maxValue = Math.max(maxValue, current);
   }
 
   for (let i = 0; i < n; i++) {
@@ -31,5 +30,6 @@ function dfs(depth) {
     result.pop();
   }
 }
+
 dfs(0);
 console.log(maxValue);
