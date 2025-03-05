f0df7d1b49176aaab962775be93cac597a59a4aa
devkoow <cwl64658@gmail.com>
2025-03-05 14:40:44 +0900

    1753 최단경로-다익스트라 업데이트

--- baekjoon/shortest distance/3/1753.js
+++ baekjoon/shortest distance/3/1753.js
@@ -3,25 +3,6 @@
 //          ↓   4
 //          3 ↗
 
-const PriorityQueue = require('../dijkstra/priorityqueue_class');
-
-function dijkstra() {
-  let pq = new PriorityQueue((a, b) => b[0] - a[0]);
-  pq.enq([0, start]);
-  distance[start] = 0;
-  while (pq.size() != 0) {
-    let [dist, now] = pq.deq();
-    if (distance[now] < dist) continue;
-    for (let i of graph[now]) {
-      let cost = dist + i[1];
-      if (cost > distance[i[0]]) {
-        distance[i[0]] = cost;
-        pq.enq([cost, i[0]]);
-      }
-    }
-  }
-}
-
 let file = `5 6
 1
 5 1 1
@@ -31,19 +12,119 @@ let file = `5 6
 2 4 5
 3 4 6`;
 let input = file.split('\n');
-let INF = 1e9;
 let [n, m] = input[0].split(' ').map(Number);
 let start = Number(input[1]);
-let graph = [];
-for (let i = 0; i <= n; i++) graph.push([]);
+let INF = 1e9;
+let graph = Array(n + 1)
+  .fill(null)
+  .map(() => []);
 for (let i = 2; i <= m + 1; i++) {
   let [a, b, c] = input[i].split(' ').map(Number);
   graph[a].push([b, c]);
 }
+
 let distance = new Array(n + 1).fill(INF);
+
+// 우선순위 큐 + 노드
+class PriorityQueue {
+  constructor() {
+    this.values = [];
+  }
+
+  enq(val, priority) {
+    const newNode = new Node(val, priority);
+    this.values.push(newNode);
+    this.bubbleUp();
+  }
+
+  bubbleUp() {
+    let index = this.values.length - 1;
+    const element = this.values[index];
+    while (index > 0) {
+      let parentIndex = Math.floor((index - 1) / 2);
+      let parent = this.values[parentIndex];
+      if (parent.priority <= element.priority) break;
+      this.values[parentIndex] = element;
+      this.values[index] = parent;
+      index = parentIndex;
+    }
+  }
+
+  deq() {
+    if (this.values.length === 0) return null; // 바로 널 반환
+    const min = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+    return min;
+  }
+
+  sinkDown() {
+    let index = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+
+    while (true) {
+      let leftChildIndex = 2 * index + 1;
+      let rightChildIndex = 2 * index + 2;
+      let leftChild;
+      let rightChild;
+      let swap = null;
+      if (leftChildIndex < length) {
+        leftChild = this.values[leftChildIndex];
+        if (leftChild.priority < element.priority) swap = leftChildIndex;
+      }
+
+      if (rightChildIndex < length) {
+        rightChild = this.values[rightChildIndex];
+        if (
+          (swap === null && rightChild.priority < element.priority) ||
+          (swap !== null && rightChild.priority < leftChild.priority)
+        ) {
+          swap = rightChildIndex;
+        }
+      }
+      if (swap === null) break;
+      this.values[index] = this.values[swap];
+      this.values[swap] = element;
+      index = swap;
+    }
+  }
+}
+
+class Node {
+  constructor(val, priority) {
+    this.val = val;
+    this.priority = priority;
+  }
+}
+
+// 다익스트라 함수
+function dijkstra() {
+  let pq = new PriorityQueue();
+  pq.enq(start, 0); // 시작 노드 넣기
+  distance[start] = 0; // 최단거리 기록
+  while (pq.values.length !== 0) {
+    let node = pq.deq();
+    let now = node.val; // 현재 노드
+    let dist = node.priority; // 현재 노드까지의 거리
+
+    if (distance[now] < dist) continue; // 최단거리가 이미 있으면 뒤의 과정은 패스하기
+
+    for (let i of graph[now]) {
+      let cost = i[1] + dist; // 현재 노드까지 거리 + 인접 노드까지의 거리
+      if (cost < distance[i[0]]) {
+        distance[i[0]] = cost; // 최단거리 갱신
+        pq.enq(i[0], cost); // 큐에 인접노드와 비용을 넣어서 재귀 반복
+      }
+    }
+  }
+}
 
 dijkstra();
 for (let i = 1; i <= n; i++) {
-  if (distance[i] == INF) console.log('INF');
+  if (distance[i] === INF) console.log('INF');
   else console.log(distance[i]);
 }
--- baekjoon/shortest distance/dijkstra/dijkstra.js
+++ baekjoon/shortest distance/dijkstra/dijkstra.js
@@ -1,31 +1,111 @@
-const PriorityQueue = require('./priorityqueue');
+// 삽입 O(log N), 삭제 O(log N), 탐색 O(N)
+class PriorityQueue {
+  constructor() {
+    this.values = [];
+  }
 
+  enq(val, priority) {
+    let newNode = new Node(val, priority);
+    this.values.push(newNode);
+    this.bubbleUp();
+  }
+
+  bubbleUp() {
+    let index = this.values.length - 1;
+    const element = this.values[index];
+    while (index > 0) {
+      let parentIndex = Math.floor((index - 1) / 2);
+      let parent = this.values[parentIndex];
+      if (element.priority >= parent.priority) break;
+      this.values[parentIndex] = element;
+      this.values[index] = parent;
+      index = parentIndex;
+    }
+  }
+
+  deq() {
+    const min = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+    return min;
+  }
+
+  sinkDown() {
+    let index = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+    while (true) {
+      let leftChildIndex = 2 * index + 1;
+      let rightChildIndex = 2 * index + 2;
+      let swap = null;
+      let leftChild;
+      let rightChild;
+
+      if (leftChildIndex < length) {
+        leftChild = this.values[leftChildIndex];
+        if (leftChild.priority < element.priority) swap = leftChildIndex;
+      }
+
+      if (rightChildIndex < length) {
+        rightChild = this.values[rightChildIndex];
+        if (
+          (swap === null && rightChild.priority < element.priority) ||
+          (swap !== null && rightChild.priority < leftChild.priority)
+        )
+          swap = rightChildIndex;
+      }
+
+      if (swap === null) break;
+      this.values[index] = this.values[swap];
+      this.values[swap] = element;
+      index = swap;
+    }
+  }
+
+  size() {
+    return console.log(this.values.length);
+  }
+}
+
+class Node {
+  constructor(val, priority) {
+    this.val = val;
+    this.priority = priority;
+  }
+}
+
+// O(E log V)
 function dijkstra() {
-  let pq = new PriorityQueue((a, b) => b[0] - a[0]); // 최소 힙
-  pq.enq([0, start]); // 시작 노드로 가기 위한 최단 거리는 0으로 우선순위 큐에 삽입
+  let pq = new PriorityQueue();
+  pq.enq(start, 0); // 시작 노드, 최단거리 0
   distance[start] = 0;
-  // 우선순위 큐가 비어있지 않다면
-  while (pq.size() != 0) {
-    let [dist, now] = pq.deq(); // 가장 최단 거리가 짧은 노드에 대한 정보 꺼내기
-    if (distance[now] < dist) continue; // 현재 노드가 이미 처리된 적이 있으면 무시하기
-    // 현재 노드와 연결된 다른 인접한 노드들을 확인하기
+  while (pq.values.length !== 0) {
+    // 가장 최단 거리가 짧은 노드 정보
+    let node = pq.deq();
+    let now = node.val;
+    let dist = node.priority;
+
+    // 현재 노드가 이미 처리된 적이 있는 노드라면 무시
+    if (distance[now] < dist) continue;
+
+    // 현재 노드를 거치는 비용과 비교하기
     for (let i of graph[now]) {
       let cost = dist + i[1];
-      // 현재 노드를 거쳐서 다른 노드로 이동하는 거리가 더 짧은 경우 구하기
+      // 현재 노드를 거쳐서 다른 노드로 이동하는 거리가 더 짧은 경우 최단거리 갱신
       if (cost < distance[i[0]]) {
         distance[i[0]] = cost;
-        pq.enq([cost, i[0]]);
+        pq.enq(i[0], cost);
       }
     }
   }
 }
 
-// 예제
-let INF = 1e9; // 무한을 의미하는 10억
-let n = 7; // 노드 개수
-let start = 1; // 시작 노드 번호
-// 각 노드에 연결되어있는 노드에 대한 정보를 담는 리스트를 만들기
-// 각 간선은 [노드, 비옹] 형태
+let INF = 1e9;
+let n = 7;
+let start = 1;
 let graph = [
   [],
   [
@@ -59,9 +139,10 @@ let graph = [
   ],
   [[4, 1]],
 ];
+
 let distance = new Array(n + 1).fill(INF);
 dijkstra();
 for (let i = 1; i <= n; i++) {
-  if (distance[i] == INF) console.log('INFINITY');
+  if (distance[i] === INF) console.log('INFINITY');
   else console.log(distance[i]);
 }
